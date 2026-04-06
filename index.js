4639aa337510b7436b3484ab29cf1ffe1173c3d5
Sergio <serocar2011@gmail.com>
2026-04-06 16:17:32 -0300

    modif index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import "dotenv/config"
 import express from "express"
 import { PORT } from "./src/config/config.js"
 import { connectDB } from "./src/config/db.js"
@@ -7,21 +8,30 @@ import categoryRoutes from "./src/routes/categoryRouter.js"
 import { handleError } from "./src/utils/errorHandler.js"
 import printJobRoutes from "./src/routes/printJobRoutes.js"
 import fileRoutes from "./src/routes/fileRoutes.js"
+import userRouter from "./src/routes/userRoutes.js"
+import fullPrintJobRouter from "./src/routes/fullPrintJobRoutes.js"
 
-    const app =express()
-    app.use(express.json())
-    app.use(express.urlencoded({extended: true}))
-    connectDB()
 
-    app.use ("/api/products", productRouter)
-    app.use("/api/clientes", clienteRoutes)
-    app.use("/api/categories", categoryRoutes)
-    app.use("/api/printjobs", printJobRoutes)
-    app.use("/api/files", fileRoutes)
+const app = express()
 
-    app.use (handleError)
 
-    app.listen(PORT,()=>{
-        console.log(`Server is running in port ${PORT}`)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-    })
+
+connectDB()
+
+app.use("/api/products", productRouter)
+app.use("/api/clientes", clienteRoutes)
+app.use("/api/categories", categoryRoutes)
+app.use("/api/printjobs", printJobRoutes)
+app.use("/api/fullprintjobs", fullPrintJobRouter)
+app.use("/api/files", fileRoutes)
+app.use("/api/user", userRouter)
+
+
+app.use(handleError)
+
+app.listen(PORT, () => {
+  console.log(`Server is running in port ${PORT}`)
+})
